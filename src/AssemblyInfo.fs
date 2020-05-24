6fe16d5fca289dd2227d1c468f4c1ea7e28d0ece
wxinix <xinwuping007@gmail.com>
2020-05-23 22:09:58 -0400

    Update AssemblyInfo

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyTitleAttribute("FSharp.Interop.ComProvider")>]
+[<assembly: AssemblyTitleAttribute("F# Type Provider for COM Interop")>]
 [<assembly: AssemblyProductAttribute("FSharp.Interop.ComProvider")>]
-[<assembly: AssemblyDescriptionAttribute("F# type provider for COM interop.")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("1.0.0")>]
 do ()
